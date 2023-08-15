4aa064efb4e6aaf50e49604e05ac4d313dca99c5
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-15 20:02:12 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,18 +1,17 @@
 import time, random, string
-def __init__(self):
-  print("Hey, Welcome to Sky's Nitro Grenerator:)")
-  time.sleep(1)
-  print("Before you begin, Do you agree to The Terms of Service shown in this program? Y/N")
-  eula = input("")
-  if eula == "Y":
-    amount = input("say a number bigger then zero and ill generate for you \n>> ")
+print("Hey, Welcome to Sky's Nitro Grenerator:)")
+time.sleep(1)
+print("Before you begin, Do you agree to The Terms of Service shown in this program? Y/N")
+eula = input("")
+if eula == "Y":
+  amount = input("say a number bigger then zero and ill generate for you \n>> ")
 
-    while amount != 0:
-      cid = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
-      print("https://discord.gift/",cid)
+  while amount != 0:
+    cid = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
+    print("https://discord.gift/",cid)
   
 
     time.sleep(1)
-  if eula == "N":
-    print("Exiting Program")
-    self.quit()
+if eula == "N":
+  print("Exiting Program")
+    
