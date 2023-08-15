392c67c0e8a73ae1d81a9dbc8e185318a3af990c
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-15 19:24:51 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import time, random, string
-amount = input("say a number bigger then zero and ill generate foryou \n>> ")
+amount = input("say a number bigger then zero and ill generate for you \n>> ")
 
 while amount != 0:
   cid = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(11))
