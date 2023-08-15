3685992ec4e181c8665a40c8462b1aa064b2e423
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-15 19:24:05 +1000

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,6 @@
+import time, random, string
+amount = input("Input any Number or letter other then 0 for me to work:)\n>> ")
+while amount != 0:
+cid = "".join(random.choice (string ascii_uppercase + string.ascii_lowercase + string.digits)for print("https://discord.gift/",_in range(11))
+cid
+time.sleep (1)
