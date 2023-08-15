424ddc47afea9f2b767ef8a2928a8f49a925ee3a
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-15 19:24:37 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 import time, random, string
-amount = input("Input any Number or letter other then 0 for me to work:)\n>> ")
+amount = input("say a number bigger then zero and ill generate foryou \n>> ")
+
 while amount != 0:
-cid = "".join(random.choice (string ascii_uppercase + string.ascii_lowercase + string.digits)for print("https://discord.gift/",_in range(11))
-cid
-time.sleep (1)
+  cid = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(11))
+  print("https://discord.gift/",cid)
+  
+
+  time.sleep(1)
